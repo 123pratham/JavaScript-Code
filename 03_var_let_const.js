b967c4a4d7f6f169ab2337d5cc7a841b1376f3bf
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2022-12-17 14:50:54 +0530

    var-let-const

--- 03_var_let_const.js
+++ 03_var_let_const.js
@@ -1,2 +1,10 @@
-let name = "Pratham Sahu"
-console.log("The name is "+name)
+// let name = "Pratham Sahu"
+// console.log("The name is "+name)
+// let name = 78  
+var a = 8
+console.log(a)
+var a = "Prtaham sahu"
+console.log(a)  // chances to introduce error
+
+const b = 56
+const b = "NAme"
