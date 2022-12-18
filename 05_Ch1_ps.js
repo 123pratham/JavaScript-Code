f125dcc5a11a641219ea52e13c1ba6c91e059482
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2022-12-18 17:03:21 +0530

    // Chapter 1 - Q4

--- 05_Ch1_ps.js
+++ 05_Ch1_ps.js
@@ -6,10 +6,18 @@ console.log(a+b)
 console.log(typeof (a+b))
 
 // Chapter 1 - Q3
-const n = {
-    name: "Pratham",
-    section :5,
-    isPrincipal: false,
+// const n = {
+//     name: "Pratham",
+//     section :5,
+//     isPrincipal: false,
 
+// }
+// n = 54
+// Chapter 1 - Q4
+const dict = {
+    antaraxia : "A state of freedom from emotional disturbance and anxiety.",
+    appreciate : " Recognize the full worth of. ",
+    yakka : "Work , especially hard work.",
 }
-n = 54
+console.log(dict.yakka)
+console.log(dict['antaraxia'])
