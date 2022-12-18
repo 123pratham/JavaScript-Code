6cb6837f7efc67c29e10741dc77a46a1e6811013
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2022-12-18 16:51:27 +0530

    // Chapter 1 - Q2

--- 05_Ch1_ps.js
+++ 05_Ch1_ps.js
@@ -4,3 +4,12 @@ let b = 4
 console.log(a+b)
 // Chapter 1 - Q2
 console.log(typeof (a+b))
+
+// Chapter 1 - Q3
+const n = {
+    name: "Pratham",
+    section :5,
+    isPrincipal: false,
+
+}
+n = 54
